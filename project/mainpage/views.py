e1e1d892c176f352aac97fbc7880a5ac258d9be2
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-10-26 14:56:16 +0300

    main news data gathered from data base and showed up

--- project/mainpage/views.py
+++ project/mainpage/views.py
@@ -4,10 +4,16 @@ from .models import *
 
 def mainpage(request):
 
-    new = news.objects.all()
+    new_left_side = news.objects.all()[:1]
+    new_right_side = news.objects.all()[1:2]
+    new_center_top = news.objects.all()[2:3]
+    new_center_bottom = news.objects.all()[3:5]
 
     context = {
-        'new' : new
+        'new_left_side' : new_left_side,
+        'new_right_side' : new_right_side,
+        'new_center_top' : new_center_top,
+        'new_center_bottom' : new_center_bottom,
     }
 
     return render(request,"index.html", context)
